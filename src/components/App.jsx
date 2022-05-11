bef4e976f51be1fda98b0ba3586030fcd5afffac
Markab-1 <velichko.anna.b@gmail.com>
2022-05-11 20:15:56 +0300

    iter-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,76 @@
+import React, { Component } from 'react';
 import Container from './Container/Container';
-import Feedback from './Feedback/Feedback';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import s from './Feedback.module.css';
 
 export const App = () => {
+  class Feedback extends Component {
+    state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+
+    onLeaveFeedback = event => {
+      const option = event.target.textContent.toLowerCase();
+
+      this.setState(prevState => {
+        return {
+          [option]: prevState[option] + 1,
+        };
+      });
+    };
+
+    countTotalFeedback({ good, neutral, bad }) {
+      return good + neutral + bad;
+    }
+
+    countPositiveFeedbackPercentage({ good, total }) {
+      return Math.round((good / total) * 100);
+    }
+
+    render() {
+      const { good, neutral, bad } = this.state;
+      const total = this.countTotalFeedback({ good, neutral, bad });
+      const positivePercentage = this.countPositiveFeedbackPercentage({
+        good,
+        total,
+      });
+
+      return (
+        <div className={s.Statistics}>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              onLeaveFeedback={this.onLeaveFeedback}
+              options={Object.keys(this.state)}
+            />
+          </Section>
+
+          <Section title="Statistics:">
+            {Object.values(this.state).some(value => {
+              return value;
+            }) ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={total}
+                positivePercentage={positivePercentage}
+              />
+            ) : (
+              <p>No feedback given</p>
+            )}
+          </Section>
+        </div>
+      );
+    }
+  }
+
   return (
     <Container>
-      <Feedback/>
-    </Container>  
+      <Feedback />
+    </Container>
   );
 };
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ options, onLeaveFeedback }) => {  
-    return (
-        <div>
-            <ul className={s.Statistics__list}>
-                <li className={s.Statistics__item}>                     
-                    <button type="button" className={s.Statistics__btn} onClick={onLeaveFeedback}>Good</button>
-                </li>                
-                <li className={s.Statistics__item}>                    
-                    <button type="button" className={s.Statistics__btn} onClick={onLeaveFeedback}>Neutral</button>                    
-                </li>                
-                <li className={s.Statistics__item}>                    
-                    <button type="button" className={s.Statistics__btn} onClick={onLeaveFeedback}>Bad</button>                    
-                </li>                
-            </ul>           
-        </div>
-    );
-}
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      <ul className={s.Statistics__list}>
+        {options.map(option => (
+          <li key={option} className={s.Statistics__item}>
+            <button
+              type="button"
+              className={s.Statistics__btn}
+              onClick={onLeaveFeedback}
+            >
+              <span className={s.text}>{option}</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+};
